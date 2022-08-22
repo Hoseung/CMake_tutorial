e311fdd57b6f8441f82ac2c93ae657cfaccfcfcc
Hoseung Choi <hschoi@dinsight.ai>
2022-08-22 14:24:39 +0900

    Use original tutorial library

--- tutorial.cpp
+++ tutorial.cpp
@@ -17,23 +17,23 @@ int main(int argc, char **argv) {
     }
 
 #ifdef USE_MYLIB
-    result = math::add(1, 2);
-    cout << "add 1 + 2 : " << result << endl;
+    // result = math::add(1, 2);
+    // cout << "add 1 + 2 : " << result << endl;
 
-    result = math::add(4, 2);
-    cout << "add 4 + 2 : " << result << endl;
+    // result = math::add(4, 2);
+    // cout << "add 4 + 2 : " << result << endl;
 
-    result = math::sub(1, 2);
-    cout << "sub 1 - 2 : " << result << endl;
+    // result = math::sub(1, 2);
+    // cout << "sub 1 - 2 : " << result << endl;
 
-    result = math::sub(4, 2);
-    cout << "sub 4 - 2 : " << result << endl;
+    // result = math::sub(4, 2);
+    // cout << "sub 4 - 2 : " << result << endl;
 
-    result = math::mul(1, 2);
-    cout << "mul 1 * 2 : " << result << endl;
+    // result = math::mul(1, 2);
+    // cout << "mul 1 * 2 : " << result << endl;
 
-    result = math::mul(4, 2);
-    cout << "mul 4 * 2 : " << result << endl;
+    // result = math::mul(4, 2);
+    // cout << "mul 4 * 2 : " << result << endl;
 #else
     cout << "No math library!" << endl;
 #endif
